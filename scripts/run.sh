0f82cfe05db6a764bb84f4bb3d3486a1378316f4
Zachary Ozer <zach@theozer.com>
2012-08-07 18:30:01 -0400

    Modifying run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=${HOME}/lib/python:$PYTHONPATH
-export PATH=${HOME}/bin:$PATH
+export PYTHONPATH=/home/41000/lib/python:$PYTHONPATH
+export PATH=/home/41000/bin:$PATH
 
 python /home/41000/data/scripts/altxkcd/src/altxkcd.py updateComic $1 $2 $3

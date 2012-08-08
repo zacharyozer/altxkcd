d859930159fbf18ac635b425b21d9863f0218874
Zachary Ozer <zach@theozer.com>
2012-08-07 23:43:27 -0400

    Cleaning up run script a bit more

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=/home/41000/lib/python:$PYTHONPATH
-export PATH=/home/41000/bin:$PATH
+export PYTHONPATH=/home/41000/users/.home/lib/python:$PYTHONPATH
+export PATH=/home/41000/users/.home/bin:$PATH
 
 python /home/41000/data/scripts/altxkcd/src/altxkcd.py

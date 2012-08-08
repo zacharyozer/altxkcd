5b02b890e2650668368464e01106609535a6bc21
Zachary Ozer <zach@theozer.com>
2012-08-07 23:38:53 -0400

    Finalizing run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 export PYTHONPATH=/home/41000/lib/python:$PYTHONPATH
 export PATH=/home/41000/bin:$PATH
 
-python /home/41000/data/scripts/altxkcd/src/altxkcd.py updateComic $1 $2 $3
+python /home/41000/data/scripts/altxkcd/src/altxkcd.py
